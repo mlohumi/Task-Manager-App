3160c2905eeed256331a11c7256a234c77ab5343
iamdeveloper <lohumi.mukesh@gmail.com>
2021-10-17 13:02:48 +0530

    middleware added

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+/* 
+===== TODO =====
+
+Custom error handling
+- Wrap all controllers in a wrapper
+- Create a custom error handling middleware
+- Create a custom error class which extends express error class
+
+**/
+
 const express = require("express")
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 // middleware
 app.use(express.static('./public'))
@@ -11,14 +22,10 @@ app.use(express.json())
 
 // routes
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 
-// app.get('/api/v1/tasks')                 - get all the tasks
-// app.post('/api/v1/tasks')                - create a new task
-// app.get('/api/v1/tasks/:id')             - get a single task
-// app.patch('/api/v1/tasks/:id')            - update task
-// app.delete('/api/v1/tasks/:id')           - delete task
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
@@ -30,3 +37,9 @@ const start = async () => {
 }
 
 start()
+
+// app.get('/api/v1/tasks')                 - get all the tasks
+// app.post('/api/v1/tasks')                - create a new task
+// app.get('/api/v1/tasks/:id')             - get a single task
+// app.patch('/api/v1/tasks/:id')            - update task
+// app.delete('/api/v1/tasks/:id')           - delete task
