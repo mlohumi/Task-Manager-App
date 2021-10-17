b1e212c1266c45276c31e68289955759f353b54d
iamdeveloper <lohumi.mukesh@gmail.com>
2021-10-17 11:18:14 +0530

    completed the basic structure

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require("express")
 const app = express()
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task manager app')
-    res.end()
-})
-
 app.use('/api/v1/tasks', tasks)
 
 // app.get('/api/v1/tasks')                 - get all the tasks
@@ -22,4 +19,14 @@ app.use('/api/v1/tasks', tasks)
 // app.delete('/api/v1/tasks/:id')           - delete task
 
 const port = 3000
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
